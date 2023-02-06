9dc0c92a13a9fed82998378524645831ca5c843b
peperoncino <2wua4nlyi@gmail.com>
2023-02-07 03:50:50 +0900

    add  article table

---
+++ hasura/migrations/default/1675709384448_create_table_public_articles/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."articles" ("id" serial NOT NULL, "user_id" integer NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON UPDATE cascade ON DELETE cascade);
